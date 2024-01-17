e438a142162441d55ad122b33d1108c849bbf190
Alex Kimeu <alexkimeu1999@gmail.com>
2024-01-18 00:27:45 +0300

    Added a Post, Patch and Delete requests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+// Event Listeners
+document.querySelector('#car-form').addEventListener('submit', handleSubmit)
+
+// Event Handlers
+function handleSubmit(e) {
+    e.preventDefault()
+    let carObj ={
+        brand: e.target.brand.value,
+        model: e.target.model.value,
+        price: e.target.price.value,
+        available: e.target.available.value,
+        imageUrl: e.target.imageUrl.value
+    }
+    renderOneCar(carObj)
+    reserveCar(carObj)
+}
+
 // DOM Render Functions
 function renderOneCar(car) {
     // Build the car card container
@@ -12,9 +29,23 @@ function renderOneCar(car) {
             <p>
                 <span class="car_count">${car.available}</span> Available
             </p>
-            <button id="reserve" >Reserve</button>
+            <div class="card_buttons" >
+                <button id="reserve" >Reserve</button>
+                <button id="remove" >Remove</button>
+            </div>
         </div> 
     `
+    card.querySelector('#reserve').addEventListener('click', () => {
+        car.available -= 1
+        card.querySelector('span').textContent = car.available
+        updateCar(car)
+    })
+    card.querySelector('#remove').addEventListener('click', () => {
+        card.remove()
+        deleteCar(car.id)
+    })
+
+    
     // Add the car card to the DOM
     document.querySelector('#car-list').appendChild(card);
 }
@@ -25,7 +56,46 @@ function getAllCars() {
     fetch('https://cars-api-m7at.onrender.com/Cars')
     .then(resp => resp.json())
     .then(carData => carData.forEach(car => renderOneCar(car)))
-}   
+} 
+
+// Post Fetch for a new car resource
+function reserveCar(carObj) {
+    fetch('https://cars-api-m7at.onrender.com/Cars', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(carObj)
+    })
+    .then(res => res.json())
+    .then(car => console.log(car))
+}
+
+// Patch Fetch for a car resource
+function updateCar(carObj) {
+    fetch(`https://cars-api-m7at.onrender.com/Cars/${carObj.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(carObj)
+    })
+    .then(res => res.json())
+    .then(car => console.log(car))
+}
+
+// Delete Fetch for a car resource
+function deleteCar(id) {
+    fetch(`https://cars-api-m7at.onrender.com/Cars/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(car => console.log(car))
+}
+
 // Initialize Render
 // Get all cars and render our cars to the DOM
 function initialize() {
