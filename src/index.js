990cbc432e3ae18afac4f3f624fa442c132a427f
Alex Kimeu <alexkimeu1999@gmail.com>
2024-01-17 16:49:55 +0300

    Added a Fetch method to get car details and render them

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+// DOM Render Functions
+function renderOneCar(car) {
+    // Build the car card container
+    let card = document.createElement('li');
+    card.className = 'card';
+    card.innerHTML = `
+        <img src="${car.imageUrl}">
+        <div class="content">
+            <h2>${car.brand}</h2>
+            <p>${car.model}</p>
+            <p>${car.price}</p>
+            <p>
+                <span class="car_count">${car.available}</span> Available
+            </p>
+            <button>Reserve</button>
+        </div> 
+    `
+    // Add the car card to the DOM
+    document.querySelector('#car-list').appendChild(card);
+}
 
+// Fetch Requests
+// Get Fetch for all car resources
+function getAllCars() {
+    fetch('http://localhost:3000/Cars')
+    .then(resp => resp.json())
+    .then(carData => carData.forEach(car => renderOneCar(car)))
+}   
+// Initialize Render
+// Get all cars and render our cars to the DOM
+function initialize() {
+    getAllCars();
+}
+initialize();
