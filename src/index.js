29db926551321fab37a88816ac4803650f5b7567
toxicanonymous253 <ngugifrancis930@gmail.com>
2025-04-10 16:12:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
     // For Mobile Menu
-    window.onload = function(){
-        const toggleButton = document.getElementsByClassName('toggle-button')[0]
-    const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-    
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
-    })
-    }
+    // window.onload = function(){
+    //     const toggleButton = document.getElementsByClassName('toggle-button')[0]
+    //     const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+        
+    //     toggleButton.addEventListener('click', () => {
+    //         navbarLinks.classList.toggle('active')
+    //     })
+    // }
 
     // For Search Button
     document.getElementById('searchButton').addEventListener('click', handleSearch);
